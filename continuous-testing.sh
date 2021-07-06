bfd06ce0335bcdd799f830b439634b4d966c60aa
Mikey Garcia <gikeymarcia@gmail.com>
2021-07-06 08:25:40 -0700

    default to verbose testing

--- continuous-testing.sh
+++ continuous-testing.sh
@@ -2,6 +2,6 @@
 
 # re-run all tests if any .py files change in the project
 while true; do
-    fd -e py | entr -rdc python -m pytest .
+    fd -e py | entr -rdc python -m pytest -v .
     sleep 1
 done
