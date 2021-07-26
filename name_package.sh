7f05d919b3d791ad453396eee01f41b0b2e07aa5
Mikey Garcia <gikeymarcia@gmail.com>
2021-07-26 15:49:26 -0700

    script to propigate project name before development

---
+++ name_package.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+# Mikey Garcia, @gikeymarcia
+# Pass package name and update referecnes throughout the project
+# Intended for use when beginning a new package
+
+if [ -n "$1" ]; then
+    name=$1
+
+    new_setup=$(mktemp)
+    sed "s/package_name/$name/" setup.py > "$new_setup"
+    mv -f "$new_setup" setup.py
+
+    mv -v ./package "./$name"
+
+    bumpversion=$(mktemp)
+    sed "s/package/$name/" .bumpversion.cfg > "$bumpversion"
+    mv -vf "$bumpversion" .bumpversion.cfg
+    git diff HEAD
+else
+    echo "No name given. No changes will be made."
+fi
--- setup.py
+++ setup.py
@@ -2,20 +2,20 @@
 from pathlib import Path
 from setuptools import setup, find_packages
 
-README = (Path(__file__).parent / "README.md").read_text()
+README = Path(__file__).parent / "README.md"
 
 
 # https://setuptools.readthedocs.io/en/latest/setuptools.html#basic-use
 setup(
-    name="package",
+    name="package_name",
     version="0.1.0",
     description="Short description of the project",
-    long_description=README,
+    long_description=README.read_text(),
     long_description_content_type="text/markdown",
     url="https://github.com/gikeymarcia/",
     author="Mikey Garcia",
     author_email="gikeymarcia@gmail.com",
-    license="GPL-3.0-or-later",
+    license="GPL-3.0",
     packages=find_packages(exclude="test"),
     install_requires=[],
 )
